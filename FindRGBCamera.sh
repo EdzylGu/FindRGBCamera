22b2742e730a17cf209fda9ec8c65c709de1411d
edzylgu <edzylgu@192.168.100.50>
2018-07-02 13:20:58 +0800

    repeat record

--- FindRGBCamera.sh
+++ FindRGBCamera.sh
@@ -7,6 +7,26 @@ IFS="$OIFS"
 for ((i=0; i<${#new_string[@]}; i++)); do
     echo ${new_string[$i]} | grep 'RGB' >> /dev/null;
     if [[ $? == 0 ]]; then
-       echo ${new_string[$i+1]}
+       camera=${new_string[$i+1]}
+       echo ${camera}
      fi
+done
+
+function record()
+{
+    video_name=`date +"%Y_%m_%d_%H:%M:%S"`
+    echo ${video_name}
+    if [ $(ls -l | grep "/*.mp4" | wc -l) -gt 4 ]
+     then
+         echo "file > 4"
+         rm -r $(ls -rt | grep "/*.mp4" | head -n1)
+     fi
+
+    do=`ffmpeg -t 30000 -s 640x480 -i ${camera} ${video_name}.mp4`
+}
+
+
+while true; do
+    record
+    sleep 30000
 done
