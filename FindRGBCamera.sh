09045cb05646b52f28d8150624a2bfe5375ba978
edzylgu <edzylgu@192.168.100.50>
2018-07-03 10:43:24 +0800

    shell script coding style

--- FindRGBCamera.sh
+++ FindRGBCamera.sh
@@ -7,9 +7,9 @@ IFS="$OIFS"
 for ((i=0; i<${#new_string[@]}; i++)); do
     echo ${new_string[$i]} | grep 'RGB' >> /dev/null;
     if [[ $? == 0 ]]; then
-       camera=${new_string[$i+1]}
-       echo ${camera}
-     fi
+	camera=${new_string[$i+1]}
+	echo ${camera}
+    fi
 done
 
 function record()
@@ -17,12 +17,12 @@ function record()
     video_name=`date +"%Y_%m_%d_%H:%M:%S"`
     echo ${video_name}
     if [ $(ls -l | grep "/*.mp4" | wc -l) -gt 59 ]
-     then
-         echo "file > 59"
-         rm -r $(ls -rt | grep "/*.mp4" | head -n1)
-     fi
+    then
+	echo "file > 59"
+	rm -r $(ls -rt | grep "/*.mp4" | head -n1)
+    fi
 
-    do=`ffmpeg -t 300 -s 640x480 -i ${camera} -vf hflip -b:v 700K ${video_name}.mp4`
+do=`ffmpeg -t 300 -s 640x480 -i ${camera} -vf hflip -b:v 700K ${video_name}.mp4`
 }
 
 
